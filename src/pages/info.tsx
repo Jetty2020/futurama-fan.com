7e745f8a7faadea7e5e81093e0ffadd8d1751bca
Jetty2020 <kmclo@naver.com>
2021-12-24 01:48:54 +0900

    :sparkles: Info 페이지 구현

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,15 +1,39 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 import { useApiData } from '../hooks';
 import { Error, Loading } from '../components/common';
+
+interface CREATOR {
+  name: string;
+  url: string;
+}
 
 const InfoPage: NextPage = () => {
   const name = 'info';
   const { data, error } = useApiData(name);
-  
+
   if (!data) return <Loading />;
   if (error) return <Error />;
-
-  return <div>InfoPage</div>;
+  
+  const innerData = data[0];
+  
+  return (
+    <div>
+      <p>{innerData.synopsis}</p>
+      <p>{innerData.yearsAired}</p>
+      <ul>
+        {innerData.creators?.map((creator: CREATOR, index: string) => {
+          return (
+            <li key={index}>
+              <Link href={creator.url}>
+                <a target="_blank">{creator.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default InfoPage;
