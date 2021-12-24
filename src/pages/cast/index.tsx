fcc25916964053abdf5422d80c90c8c1b87f36f0
Jetty2020 <kmclo@naver.com>
2021-12-25 00:32:51 +0900

    :sparkles: Cast 페이지 API 연결

---
+++ src/pages/cast/index.tsx
@@ -0,0 +1,40 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { Error, Loading } from '../../components/common';
+import { useApiData } from '../../hooks';
+import { CastType } from '../../types/Cast';
+
+const CastPage: NextPage = () => {
+  const path = 'cast';
+  const { data, error } = useApiData(path);
+
+  if (!data) return <Loading />;
+  if (error) return <Error />;
+
+  return (
+    <div>
+      <ul>
+        {data.map((cast: CastType) => {
+          const { id, name, born, died, bio } = cast;
+          return (
+            <li key={`${id}`}>
+              <h2>
+                <Link href={bio.url}>
+                  <a target="_blank">{name}</a>
+                </Link>
+              </h2>
+              <p>
+                {born} ~ {died}
+              </p>
+              {
+                bio.text && <p>{bio.text}</p>
+              }
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default CastPage;
---
+++ src/types/Cast.ts
@@ -0,0 +1,10 @@
+export interface CastType {
+  id: number;
+  name: string;
+  born: string;
+  died: string;
+  bio: {
+    text: string;
+    url: string;
+  };
+}
