9aecde97be69a9a09dbdacc2e70d9688c23fd306
JeongHoon Park <kmclo@naver.com>
2021-12-29 08:32:19 +0900

    :sparkles: Open Graph 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -10,6 +16,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            property="og:url"
+            content="https://futurama-fan-site.vercel.app/"
+          />
+          <meta property="og:title" content="Futurama fan site" />
+          <meta property="og:description" content="Enjoy your Futurama" />
+          <meta property="og:image" content="/landing.jpg" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
